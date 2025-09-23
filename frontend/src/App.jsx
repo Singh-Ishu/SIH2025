f4515fe8f768a1feb462625ed8833c5456ef3bd7
Singh-Ishu <itzshaansingh@gmail.com>
2025-09-23 11:02:42 +0530

    Add Landing to the App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import Background from "../pages/Background/Background";
+import Landing from "../pages/Landing/Landing";
 import "./App.css";
 
 function App() {
     return (
         <div className="App">
             <Background />
+            <Landing />
         </div>
     );
 }
