132f3dfcdfaf54cf38ebbfecffa539b19df28f7f
Singh-Ishu <itzshaansingh@gmail.com>
2025-09-23 18:59:04 +0530

    Minor UI Updates

--- frontend/pages/Landing/Landing.jsx
+++ frontend/pages/Landing/Landing.jsx
@@ -4,17 +4,19 @@ import styles from "./Landing.module.css";
 function Landing() {
     return (
         <div className={styles["landing-container"]}>
-            <TextType
+            <h1 className={styles["landing-title"]}>WIPER</h1>
+            {/* <TextType
                 text={[
-                    "The Ultimate Data Wiping Solution",
-                    "Clean, Purge or Exterminate",
-                    "Securely Erase Your Data. FOREVER",
+                    "The Ultimate Data Wiping Solution.",
+                    "Clean, Purge or Exterminate.",
+                    "Securely Erase Your Data. FOREVER.",
                 ]}
                 typingSpeed={75}
                 pauseDuration={1500}
                 showCursor={true}
                 cursorCharacter="|"
-            />
+                startOnVisible={true}
+            /> */}
         </div>
     );
 }
