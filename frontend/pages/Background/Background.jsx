8580b771c122860dd0e7f4208354573f837becba
Singh-Ishu <itzshaansingh@gmail.com>
2025-09-23 10:44:42 +0530

    Create Background component

---
+++ frontend/pages/Background/Background.jsx
@@ -0,0 +1,17 @@
+import LetterGlitch from "../../components/layout/LetterGlitch/LetterGlitch";
+import styles from "./Background.module.css";
+
+function Background() {
+    return (
+        <div className={styles["background"]}>
+            <LetterGlitch
+                glitchSpeed={50}
+                centerVignette={true}
+                outerVignette={false}
+                smooth={true}
+            />
+        </div>
+    );
+}
+
+export default Background;
