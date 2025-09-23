7e4fffe53957c097b5d19fc1f2685ec2de945296
Singh-Ishu <itzshaansingh@gmail.com>
2025-09-23 10:44:52 +0530

    ADD Background to App

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import Background from "../pages/Background/Background";
+import "./App.css";
+
+function App() {
+    return (
+        <div className="App">
+            <Background />
+        </div>
+    );
+}
+export default App;
